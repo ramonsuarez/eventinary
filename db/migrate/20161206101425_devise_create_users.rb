942ce7b09004a079bd230330cda09cdeefe2b47f
Dugagjin Ramani <r.dugzino@gmail.com>
2016-12-06 14:12:21 +0100

    - first/last name from registrations

--- db/migrate/20161206101425_devise_create_users.rb
+++ db/migrate/20161206101425_devise_create_users.rb
@@ -30,8 +30,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :first_name, null: false
-      t.string :last_name, null: false
+      t.string :first_name
+      t.string :last_name
       t.string :address
       t.string :phone
       t.text :avatar
