5f21fd42783b70049569c016638e93489aedd58e
Luay Adam <luay985@gmail.com>
2016-12-08 18:55:17 +0100

    initial user controller

---
+++ app/controllers/concerns/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(user_params)
+  end
+
+private
+  # Doc requires this but where do we use it?
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :address, :phone, :avatar)
+  end
+
+end
