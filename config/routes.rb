3aba13cc50f6a4ba3639d2ccc9ab56a06f41cadd
luciana diehl <lu.diehl@gmail.com>
2016-12-09 14:28:07 +0100

    updated application_controller, updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
-  get 'organizers/new'
-
-  get 'organizers/create'
-
-  get 'organizers/show'
-
-  get 'organizers/edit'
-
-  get 'organizers/update'
-
-  resources :events
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Attachinary::Engine => "/attachinary"
+  resources :organization
+  resources :events do
+    resources :bookings
+  end
 end
