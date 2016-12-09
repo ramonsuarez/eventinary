86cbace8bdb851bfd2aa4411f66569215db5d32c
luciana diehl <lu.diehl@gmail.com>
2016-12-09 14:44:44 +0100

    routes small typo

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Attachinary::Engine => "/attachinary"
-  resources :organization
+  resources :organizations
   resources :events do
     resources :bookings
   end
