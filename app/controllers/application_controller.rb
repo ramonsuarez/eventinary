7e3cf36f3154c8a761caf84805def91bfe23772a
Ramon Suarez <ramon@betacowork.com>
2016-12-09 15:40:47 +0100

    remove before_action :store_previous_url from ApplicationsController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_filter :configure_permitted_parameters, if: :devise_controller?
-  before_action :store_previous_url
+#  before_action :store_previous_url
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
