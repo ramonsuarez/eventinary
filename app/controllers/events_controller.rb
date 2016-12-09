010907040b4d6a73b1d5298a1d57b1a58afd272a
Ramon Suarez <ramon@betacowork.com>
2016-12-09 14:52:18 +0100

    Show each event to non logged users

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
-
+  skip_before_action :authenticate_user!, only: [ :show ]
   # GET /events
   # GET /events.json
   def index
