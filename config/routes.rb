d9239b19090427e14e7a8e4d04e4c0d808bf2d93
Ramon Suarez <ramon@betacowork.com>
2016-12-11 19:26:29 +0100

    Bad integration in navbar, doesn't show results and is ugly

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users, only: [:show, :update, :edit]
   root to: 'pages#home'
+  resources :pages, only: [:home]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :organizations
   resources :events do
