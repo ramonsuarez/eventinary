ac245371e6192cfd1c296dae118f246148cdabd5
Ramon Suarez <ramon@betacowork.com>
2016-12-11 17:55:03 +0100

    Add pg_search

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
   has_attachment :image
-
+  include PgSearch
+    multisearchable against: [:title, :description, :venue, :address]
   has_many :bookings
-  belongs_to :organization
+  belongs_to :organizatison
 end
