60e5258b202927ab372facff04fee7b5db12851a
luciana diehl <lu.diehl@gmail.com>
2016-12-12 13:40:01 +0100

    merge conflict routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   mount Attachinary::Engine => "/attachinary"
-<<<<<<< HEAD
   mount Bootsy::Engine => '/bootsy', as: 'bootsy'
 
   devise_for :users, :controllers => { registrations: 'registrations' }
-=======
-  # devise_for :users, :controllers => { registrations: 'registrations' }
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
-
->>>>>>> b8f52d0e186c213f1b58d38d5acfbd4948b412cc
   resources :users, only: [:show, :update, :edit]
   root to: 'pages#home'
   resources :pages, only: [:home]
