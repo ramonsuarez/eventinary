b9b38dee6c394335a2adc32007f4559fd80b83f5
luciana diehl <lu.diehl@gmail.com>
2016-12-15 14:50:31 +0100

    footer as sticky, new event form look improved

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,6 +1,6 @@
 module EventsHelper
 
   def cancellation_policy_collection
-    [['No charge if attendee cancels RSVP 24 hours prior the event', 'Flexible'], ['No charge if attendee cancels RSVP 2 days prior the event', "Moderate"], ['No charge will apply if attendee cancels RSVP 7 days prior the event', 'Strict'] ]
+    [['Flexible', 'Flexible'], ['Moderate', "Moderate"], ['Strict', 'Strict'] ]
   end
 end
