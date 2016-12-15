8d1dd832c65210f7578b9aba3cd30bb7fc618251
luciana diehl <lu.diehl@gmail.com>
2016-12-15 13:04:08 +0100

    first steps form

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,6 +1,6 @@
 module EventsHelper
 
   def cancellation_policy_collection
-    [['Flexible: No charge if attendee cancels RSVP 24 hours prior the event', 'Flexible'], ['Moderate: No charge if attendee cancels RSVP 2 days prior the event', "Moderate"], ['Strict: No charge will apply if attendee cancels RSVP 7 days prior the event', 'Strict'] ]
+    [['No charge if attendee cancels RSVP 24 hours prior the event', 'Flexible'], ['No charge if attendee cancels RSVP 2 days prior the event', "Moderate"], ['No charge will apply if attendee cancels RSVP 7 days prior the event', 'Strict'] ]
   end
 end
