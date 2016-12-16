66ed2ea02cd2e69cca6db471b538f7cc765e61ae
Ramon Suarez <ramon@betacowork.com>
2016-12-16 14:52:52 +0100

    Check attendees, update parvis addresses

--- db/seeds.rb
+++ db/seeds.rb
@@ -116,7 +116,7 @@ p "User id #{u8.id}"
 u9 = User.create(
   first_name: "Anne",
   last_name: "Collet",
-  address: "Place St. Gudule 5, 1000, Brussels",
+  address: "Parvis Sainte-Gudule 5, 1000 Bruxelles",
   phone: "(+32) 476 785 764",
   email:"anne@lewagon.org",
   password: "annecollet"
@@ -262,7 +262,7 @@ p "Organization id #{o7.id}"
    offline: true,
    online_url: "",
    venue: "co.station",
-   address: "Place Sainte-Gudule 5",
+   address: "Parvis Sainte-Gudule 5, 1000 Bruxelles",
    start: "Fri, 9 Oct 2017 18:00:00 UTC +00:00",
    end: "Sun, 11 Oct 2017 20:00:00 UTC +00:00",
    description: "Brace yourselves, Belgium's first ever FinTech Startup Weekend is here - bringing the FinTech movement to the Capital of Europe! Financial technology, also known as FinTech, is using software to provide financial services. FinTech companies are generally startups founded with the purpose of disrupting incumbent financial systems and providing services such as mobile payments, money transfers, loans, fundraising and even asset management. You too, can be a Fintech innovator, whatever your background, and a Startup Weekend Maker is an excellent place to start. You will have access to financial experts, software packages, and open source innovation to prototype your idea. You will: Pitch your idea (if you want to; its an open stage format) Form a team around an idea and make it real Get grilled and coached by some of the toughest industry experts Present to a panel of expert judges and win amazing prizes Test the limits of sleeplessness ;) This will be the 1st event of its kind in Belgium where the local startup ecosystem will join in for a three day marathon of co-creation, making and growth-hacking! Check out the Startup Weekend trailer here and a first-timer's experience here. Given the elaborate set up involved, places are limited so book your spot NOW! All Startup Weekend events follow the same basic model: anyone is welcome to pitch their startup idea and receive feedback from their peers. Teams organically form around the top ideas (as determined by popular vote) and then it’s a weekend frenzy of business model creation, product designing, prototyping, business coaching and market validation. The weekends culminate with presentations in front of local entrepreneurial leaders with another opportunity for critical feedback. It isn’t always pretty. It will be challenging and can be grueling. But, if you can stick it out, you will have gained the equivalent of months worth of learning, building, and networking in one weekend. Hell, you might even start your first company! Whether you start companies, find a co-founder, meet someone new, or learn a skill far outside the usual 9-to-5, everyone is guaranteed to leave the event better prepared to navigate the chaotic but fun world of startups. The event will be held in English. The venue is quiet central so there will be a lot of hostel options around too. Check out www.hostelsclub.com and www.hostelworld.com for hostel options, and www.airbnb.com for apartments. Looking forward to seeing you all there! Team SW Brussels FinTech _________________________________ Refund Policy: No refunds will be granted within 7 days of the event as food and drinks are already ordered by then. About Startup Weekend: Startup Weekends are weekend events designed to provide superior experiential education for technical and non-technical entrepreneurs. Beginning with Friday night pitches and continuing through brainstorming, business plan development, and basic prototype creation, Startup Weekends culminate in Sunday night demos and presentations. Participants create working startups during the event and are able to collaborate with like-minded individuals outside of their daily networks. All teams hear talks by industry leaders and receive valuable feedback from local entrepreneurials. The weekend is centered around action, innovation, and education. Whether you are looking for feedback on a idea, a co-founder, specific skill sets, or a team to help you execute, Startup Weekends are the perfect environment in which to test your idea and take the first steps towards launching your own startup. Terms of Participation Agreement: It is our job to provide you with an incredible networking and learning opportunity. Our intention is that you meet some amazing people that you might actually start a company with, build relationships with coaches, and learn from your peers. The event is intended to be a collaborative forum for sharing, learning, building, and having fun. As such, by registering as a participant, you acknowledge that any ideas shared by you or anyone else over the course of the event are contributions to the overall experience and community. If you as a participant are worried about someone stealing your ideas, please reconsider your participation in the event itself or simply refrain from sharing specifics about your idea. As a member of your startup community, you agree to to be inclusive of any and all people and their ideas. You agree to act professionally, treat everyone with respect, and treat the event facilities with proper care. You agree that you are responsible for any accidents or damages that you cause. You also agree to not hold Startup Weekend, your local volunteer team, or any of the event supporters liable for any loss, damage, injury, or any other unforeseeable incident. By participating in the Startup Weekend, each Participant grants STARTUP WEEKEND and its licensees, successors and assigns a non-exclusive, perpetual, non-cancelable, royalty free, fully paid up, worldwide right and license to edit, modify, adapt, translate, exhibit, publish, transmit, participate in the transfer of, reproduce, copy, create derivative works from, distribute, perform, display and otherwise use all and/or any part of such Participant’s names, likenesses, photographs, voices and images of all persons referenced and/or appearing in materials generated at the event, without limitation, anywhere in the world in any form (tangible or intangible, permanent or ephemeral, including, without limitation, audio, video, print, electronic, written, and photographic) and via any transmission medium (including, without limitation, television, radio, Internet, and print) now existing or that may exist in the future for advertising, trade, promotion, publicity and other related purposes without compensation and without notice to or consideration, review or approval from Participant. Each Participant further agrees to (i) be questioned or interviewed (whether in person or by other means such as email or phone), filmed and/or photographed and available for interviewing, filming, photography and for other public relations, marketing and promotional activities associated with the Event, as reasonably required and directed by STARTUP WEEKEND, (ii) allow STARTUP WEEKEND to make contact with the Participant (whether in person, by phone, email or otherwise) for the purpose of requesting further information from the Participant in relation to their participation or for any other purpose related to the Startup Weekend. Each Participant represents and warrants that neither its participation, nor STARTUP WEEKEND’s exercise of any of the rights granted to it under these Terms will (a) infringe or violate any rights of any third party or entity, including, without limitation, those relating to patents, copyrights, trademarks, trade secrets, moral rights, mask works, defamation, privacy, publicity, false light, misappropriation, confidentiality, and/or any contractual or other rights recognizable under any applicable law, rule, regulation, ordinance, judgment or decree (collectively, “Law”), or (b) otherwise violate any applicable Law. By submitting your information above you agree to the UP.co family of website's Privacy Policy, and you consent to receive email communication from UP Global and its programs about events and updates that may interest you.",
@@ -680,7 +680,7 @@ b14.save
 p "Booking id #{b14.id}"
 
 b15 = Booking.create(
-  attended: false,
+  attended: true,
   cancelled: true,
   user_id: u2.id,
   event_id: e10.id
@@ -698,7 +698,7 @@ b16.save
 p "Booking id #{b16.id}"
 
 b17 = Booking.create(
-  attended: false,
+  attended: true,
   cancelled: true,
   user_id: u4.id,
   event_id: e10.id
@@ -707,7 +707,7 @@ b17.save
 p "Booking id #{b17.id}"
 
 b18 = Booking.create(
-  attended: false,
+  attended: true,
   cancelled: true,
   user_id: u5.id,
   event_id: e10.id
@@ -716,7 +716,7 @@ b18.save
 p "Booking id #{b18.id}"
 
 b19 = Booking.create(
-  attended: false,
+  attended: true,
   cancelled: true,
   user_id: u6.id,
   event_id: e10.id
@@ -725,7 +725,7 @@ b19.save
 p "Booking id #{b19.id}"
 
 b20 = Booking.create(
-  attended: false,
+  attended: true,
   cancelled: true,
   user_id: u7.id,
   event_id: e10.id
@@ -734,7 +734,7 @@ b20.save
 p "Booking id #{b20.id}"
 
 b21 = Booking.create(
-  attended: false,
+  attended: true,
   cancelled: true,
   user_id: u8.id,
   event_id: e10.id
@@ -752,7 +752,7 @@ b22.save
 p "Booking id #{b22.id}"
 
 b23 = Booking.create(
-  attended: false,
+  attended: true,
   cancelled: true,
   user_id: u10.id,
   event_id: e10.id
